f3721d329506916e02b27fb5512d65d6f0efa9a2
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-26 18:29:00 +0100

    [getMyRights] wrap vector<user_right*> in UserRightVector

--- features/java/src/test/java/gradle/cucumber/UserManagementdefs.java
+++ features/java/src/test/java/gradle/cucumber/UserManagementdefs.java
@@ -81,7 +81,7 @@ public class UserManagementdefs {
         Assert.assertFalse(k.getAuth().checkToken(k.getJwt()).getValid());
     }
 
-    @Given("^I update my user custom data with the pair \'([^\"]*)\':\'([^\"]*)\'$")
+    @Given("^I update my user custom data with the pair '(.*)':'(.*)'$")
     public void i_update_my_user_custom_data_with_the_pair(String fieldname, String fieldvalue) throws Exception {
         k.getAuth().updateSelf("{\""+fieldname+"\": \""+fieldvalue+"\"}");
     }
@@ -91,7 +91,7 @@ public class UserManagementdefs {
         currentUser = k.getAuth().getCurrentUser();
     }
 
-    @Then("^the response '_source' field contains the pair \'([^\"]*)\':\'([^\"]*)\'$")
+    @Then("^the response '_source' field contains the pair '(.*)':'(.*)'$")
     public void the_response__source_field_contains_the_pair(String key, String value) throws Exception {
         Assert.assertNotNull(currentUser.getContent());
     }
@@ -100,4 +100,10 @@ public class UserManagementdefs {
     public void i_logout() throws Exception {
         k.getAuth().logout();
     }
+
+    @Given("^I get my rights$")
+    public void i_get_my_rights() throws Exception {
+
+    }
+
 }
