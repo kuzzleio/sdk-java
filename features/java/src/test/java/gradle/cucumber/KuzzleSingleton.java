d5044e18dc0ce3d1d7a4e853f5312ce757feabba
jenow <jenova.ke20@gmail.com>
2018-11-26 15:48:24 +0100

    fix tests

--- features/java/src/test/java/gradle/cucumber/KuzzleSingleton.java
+++ features/java/src/test/java/gradle/cucumber/KuzzleSingleton.java
@@ -5,13 +5,15 @@ import io.kuzzle.sdk.WebSocket;
 
 public class KuzzleSingleton {
     private static Kuzzle kuzzle = null;
+    private static WebSocket socket;
 
     public static Kuzzle getInstance() {
         if (kuzzle != null) {
             return kuzzle;
         }
 
-        kuzzle = new Kuzzle(new WebSocket((System.getenv().get("KUZZLE_HOST") != null) ? (System.getenv().get("KUZZLE_HOST")) : "localhost"));
+        socket = new WebSocket((System.getenv().get("KUZZLE_HOST") != null) ? (System.getenv().get("KUZZLE_HOST")) : "localhost");
+        kuzzle = new Kuzzle(socket);
         return kuzzle;
     }
 }
