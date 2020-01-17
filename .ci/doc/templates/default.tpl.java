5702e00338e3b9b0e859f2d75f5069c54aee9fba
jenow <jenova.ke20@gmail.com>
2020-01-17 12:25:23 +0100

    wip

--- .ci/doc/templates/default.tpl.java
+++ .ci/doc/templates/default.tpl.java
@@ -1,1 +1,18 @@
-[snippet-code]
+import io.kuzzle.sdk.Kuzzle;
+import io.kuzzle.sdk.Protocol.WebSocket;
+import io.kuzzle.sdk.Options.KuzzleOptions;
+import java.util.concurrent.ConcurrentHashMap;
+import io.kuzzle.sdk.CoreClasses.Responses.Response;
+
+public class SnippetTest {
+  public static void main(String[] argv) {
+    try {
+      Kuzzle kuzzle = new Kuzzle(new WebSocket("kuzzle"));
+      kuzzle.connect();
+      [snippet-code]
+      System.out.println("Success");
+    } catch (Exception e) {
+      System.err.println((e.getMessage()));
+    }
+  }
+}
--- .ci/doc/templates/without-connect.tpl.java
+++ .ci/doc/templates/without-connect.tpl.java
@@ -1,13 +1,13 @@
-package test.example.java;
-
 import io.kuzzle.sdk.Kuzzle;
 import io.kuzzle.sdk.Protocol.WebSocket;
 
 public class SnippetTest {
   public static void main(String[] argv) {
     try {
-      Kuzzle kuzzle = new Kuzzle(new WebSocket("localhost"));
+      Kuzzle kuzzle = new Kuzzle(new WebSocket("kuzzle"));
       [snippet-code]
+      System.out.println("Success");
+      kuzzle.disconnect();
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
