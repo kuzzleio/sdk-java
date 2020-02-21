c9db9d8f551b9663a7c4526c4d089f35e8470fc5
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-02-21 13:52:02 +0100

    snippet

--- doc/3/controllers/document/get/snippets/get.java
+++ doc/3/controllers/document/get/snippets/get.java
@@ -1,9 +1,3 @@
-ConcurrentHashMap<String, Object> document = new ConcurrentHashMap<>();
-    document.put("name", "John");
-
-    kuzzle.getDocumentController().create("nyc-open-data", "yellow-taxi", document, "some-id")
-    .get();
-
     ConcurrentHashMap<String, Object> response =
     kuzzle.getDocumentController().get("nyc-open-data", "yellow-taxi", "some-id")
     .get();
