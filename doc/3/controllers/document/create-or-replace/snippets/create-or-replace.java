12b11759e79a766458ba3b38c37fed30ac6bf43e
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-02-28 13:21:47 +0100

    @aschen requested change

--- doc/3/controllers/document/create-or-replace/snippets/create-or-replace.java
+++ doc/3/controllers/document/create-or-replace/snippets/create-or-replace.java
@@ -1,8 +1,18 @@
 
   ConcurrentHashMap<String, Object> document = new ConcurrentHashMap<>();
   document.put("firstname", "John");
-  document.put("lastname", "Smith");
 
-  ConcurrentHashMap<String, Object> response = kuzzle.getDocumentController().createOrReplace("nyc-open-data", "yellow-taxi", "some-id", document)
+  ConcurrentHashMap<String, Object> result = kuzzle.getDocumentController().createOrReplace("nyc-open-data", "yellow-taxi", "some-id", document)
   .get();
-  System.out.println(response);
+
+/*
+    {
+      _source=
+        {
+          firstname=John,
+          _kuzzle_info={ createdAt=1582892323254, author=-1, updatedAt=1582892323254, updater=-1}
+        },
+    _id=some-id,
+    _version=1
+    }
+ */
