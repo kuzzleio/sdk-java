6513ba83ef924d9c10e234e319d56715bb9f4165
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-02-28 13:25:28 +0100

    @aschen requested change

--- doc/3/controllers/document/replace/snippets/replace.java
+++ doc/3/controllers/document/replace/snippets/replace.java
@@ -1,8 +1,19 @@
 
   ConcurrentHashMap<String, Object> document = new ConcurrentHashMap<>();
   document.put("firstname", "John");
-  document.put("lastname", "Smith");
 
-  ConcurrentHashMap<String, Object> response = kuzzle.getDocumentController().replace("nyc-open-data", "yellow-taxi", "some-id", document)
+  ConcurrentHashMap<String, Object> result = kuzzle.getDocumentController().replace("nyc-open-data", "yellow-taxi", "some-id", document)
   .get();
-  System.out.println(response);
+
+  /*
+  result =
+      {
+          _source=
+              {
+                firstname=John,
+                _kuzzle_info={ createdAt=1582892606555, author=-1, updatedAt=1582892606555, updater=-1 }
+              },
+          _id=some-id,
+          _version=2
+      }
+   */
