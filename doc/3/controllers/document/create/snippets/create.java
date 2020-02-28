5c64a9a8302291b44ee14a00d7d4d74540ee4ac1
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-02-28 13:11:18 +0100

    @aschen requested change

--- doc/3/controllers/document/create/snippets/create.java
+++ doc/3/controllers/document/create/snippets/create.java
@@ -1,7 +1,18 @@
 
   ConcurrentHashMap<String, Object> document = new ConcurrentHashMap<>();
   document.put("firstname", "John");
-  document.put("lastname", "Smith");
 
-  kuzzle.getDocumentController().create("nyc-open-data", "yellow-taxi", document)
+  ConcurrentHashMap<String, Object> result = kuzzle.getDocumentController().create("nyc-open-data", "yellow-taxi", document)
   .get();
+
+/* result =
+        {
+          _source=
+              {
+              firstname=John,
+              _kuzzle_info={ createdAt=1582891678488, author=-1 }
+              },
+          _id=mDmyi3ABYMAjFJ_ZO-EZ,
+          _version=1
+        }
+*/
