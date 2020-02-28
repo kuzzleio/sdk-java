70c31acbd0c2325628eff89a55d8fd35af880994
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-02-28 13:17:30 +0100

    @aschen requested change

--- doc/3/controllers/document/get/snippets/get.java
+++ doc/3/controllers/document/get/snippets/get.java
@@ -1,4 +1,16 @@
-    ConcurrentHashMap<String, Object> response =
+    ConcurrentHashMap<String, Object> result =
     kuzzle.getDocumentController().get("nyc-open-data", "yellow-taxi", "some-id")
     .get();
-    System.out.println(response);
+
+/*
+        {
+          _source=
+            {
+              key1=value1,
+              key2=value2,
+              _kuzzle_info= { createdAt=1582892127577, author=-1}
+            },
+          _id=some-id,
+          _version=1
+        }
+*/
