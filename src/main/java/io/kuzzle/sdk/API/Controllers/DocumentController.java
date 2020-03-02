b00e7c6b9a7d9dbf6098c13e6b809e8da67567e2
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2020-03-02 13:08:25 +0100

    Add document:replace (#62)
    What does this PR do ?
    This PR implements the document:replace method with its unit tests.

--- src/main/java/io/kuzzle/sdk/API/Controllers/DocumentController.java
+++ src/main/java/io/kuzzle/sdk/API/Controllers/DocumentController.java
@@ -15,6 +15,62 @@ public class DocumentController extends BaseController {
   }
 
   /**
+   * Replace a document in a given collection and index.
+   *
+   * @param index
+   * @param collection
+   * @param id
+   * @param document
+   * @param waitForRefresh
+   * @return a CompletableFuture
+   * @throws NotConnectedException
+   * @throws InternalException
+   */
+  public CompletableFuture<ConcurrentHashMap<String, Object>> replace(
+      final String index,
+      final String collection,
+      final String id,
+      final ConcurrentHashMap<String, Object> document,
+      final Boolean waitForRefresh) throws NotConnectedException, InternalException {
+
+    final KuzzleMap query = new KuzzleMap();
+
+    query
+        .put("index", index)
+        .put("collection", collection)
+        .put("controller", "document")
+        .put("action", "replace")
+        .put("body", document)
+        .put("_id",  id)
+        .put("waitForRefresh", waitForRefresh);
+
+    return kuzzle
+        .query(query)
+        .thenApplyAsync(
+            (response) -> (ConcurrentHashMap<String, Object>) response.result);
+  }
+
+  /**
+   * Replace a document in a given collection and index.
+   *
+   * @param index
+   * @param collection
+   * @param id
+   * @param document
+   * @return a CompletableFuture
+   * @throws NotConnectedException
+   * @throws InternalException
+   */
+  public CompletableFuture<ConcurrentHashMap<String, Object>> replace(
+      final String index,
+      final String collection,
+      final String id,
+      final ConcurrentHashMap<String, Object> document) throws NotConnectedException, InternalException {
+
+    return this.replace(index, collection, id, document, null);
+  }
+
+  /**
    * Deletes a document in a given collection and index.
    *
    * @param index
