95a805c4459ffc77f76382925637dc5150544532
Yoann <yoann1.abbes@epitech.eu>
2020-03-02 11:36:46 +0100

    conflict [ci skip]

--- src/main/java/io/kuzzle/sdk/API/Controllers/DocumentController.java
+++ src/main/java/io/kuzzle/sdk/API/Controllers/DocumentController.java
@@ -10,58 +10,58 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.UUID;
 
 public class DocumentController extends BaseController {
-  public DocumentController(final Kuzzle kuzzle) {
-    super(kuzzle);
-  }
+  // public DocumentController(final Kuzzle kuzzle) {
+  //   super(kuzzle);
+  // }
 
-  /**
-   * Deletes a document in a given collection and index.
-   *
-   * @param index
-   * @param collection
-   * @param id
-   * @param waitForRefresh
-   * @return a CompletableFuture
-   * @throws NotConnectedException
-   * @throws InternalException
-   */
-  public CompletableFuture<ConcurrentHashMap<String, Object>> delete(
-      final String index,
-      final String collection,
-      final String id,
-      final Boolean waitForRefresh) throws NotConnectedException, InternalException {
+  // /**
+  //  * Deletes a document in a given collection and index.
+  //  *
+  //  * @param index
+  //  * @param collection
+  //  * @param id
+  //  * @param waitForRefresh
+  //  * @return a CompletableFuture
+  //  * @throws NotConnectedException
+  //  * @throws InternalException
+  //  */
+  // public CompletableFuture<ConcurrentHashMap<String, Object>> delete(
+  //     final String index,
+  //     final String collection,
+  //     final String id,
+  //     final Boolean waitForRefresh) throws NotConnectedException, InternalException {
 
-    final KuzzleMap query = new KuzzleMap();
+  //   final KuzzleMap query = new KuzzleMap();
 
-    query
-        .put("index", index)
-        .put("collection", collection)
-        .put("controller", "document")
-        .put("action", "delete")
-        .put("_id",  id)
-        .put("waitForRefresh", waitForRefresh);
+  //   query
+  //       .put("index", index)
+  //       .put("collection", collection)
+  //       .put("controller", "document")
+  //       .put("action", "delete")
+  //       .put("_id",  id)
+  //       .put("waitForRefresh", waitForRefresh);
 
-    return kuzzle
-        .query(query)
-        .thenApplyAsync(
-            (response) -> (ConcurrentHashMap<String, Object>) response.result);
-  }
+  //   return kuzzle
+  //       .query(query)
+  //       .thenApplyAsync(
+  //           (response) -> (ConcurrentHashMap<String, Object>) response.result);
+  // }
 
-  /**
-   * Deletes a document in a given collection and index.
-   *
-   * @param index
-   * @param collection
-   * @param id
-   * @return a CompletableFuture
-   * @throws NotConnectedException
-   * @throws InternalException
-   */
-  public CompletableFuture<ConcurrentHashMap<String, Object>> delete(
-      final String index,
-      final String collection,
-      final String id) throws NotConnectedException, InternalException {
+  // /**
+  //  * Deletes a document in a given collection and index.
+  //  *
+  //  * @param index
+  //  * @param collection
+  //  * @param id
+  //  * @return a CompletableFuture
+  //  * @throws NotConnectedException
+  //  * @throws InternalException
+  //  */
+  // public CompletableFuture<ConcurrentHashMap<String, Object>> delete(
+  //     final String index,
+  //     final String collection,
+  //     final String id) throws NotConnectedException, InternalException {
 
-    return this.delete(index, collection, id, null);
-  }
+  //   return this.delete(index, collection, id, null);
+  // }
 }
--- src/test/java/io/kuzzle/test/API/Controllers/DocumentTest/DocumentTest.java
+++ src/test/java/io/kuzzle/test/API/Controllers/DocumentTest/DocumentTest.java
@@ -22,53 +22,53 @@ public class DocumentTest {
 
   private AbstractProtocol networkProtocol = Mockito.mock(WebSocket.class);
 
-  @Test
-  public void deleteDocumentTestA() throws NotConnectedException, InternalException {
+  // @Test
+  // public void deleteDocumentTestA() throws NotConnectedException, InternalException {
 
-    Kuzzle kuzzleMock = spy(new Kuzzle(networkProtocol));
-    String index = "nyc-open-data";
-    String collection = "yellow-taxi";
+  //   Kuzzle kuzzleMock = spy(new Kuzzle(networkProtocol));
+  //   String index = "nyc-open-data";
+  //   String collection = "yellow-taxi";
 
-    ArgumentCaptor arg = ArgumentCaptor.forClass(KuzzleMap.class);
+  //   ArgumentCaptor arg = ArgumentCaptor.forClass(KuzzleMap.class);
 
-    kuzzleMock.getDocumentController().delete(index, collection, "some-id", true);
-    Mockito.verify(kuzzleMock, Mockito.times(1)).query((KuzzleMap) arg.capture());
+  //   kuzzleMock.getDocumentController().delete(index, collection, "some-id", true);
+  //   Mockito.verify(kuzzleMock, Mockito.times(1)).query((KuzzleMap) arg.capture());
 
-    assertEquals(((KuzzleMap) arg.getValue()).getString("controller"), "document");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("action"), "delete");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("index"), "nyc-open-data");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("_id"), "some-id");
-    assertEquals(((KuzzleMap) arg.getValue()).getBoolean("waitForRefresh"), true);
-  }
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("controller"), "document");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("action"), "delete");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("index"), "nyc-open-data");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("_id"), "some-id");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getBoolean("waitForRefresh"), true);
+  // }
 
-  @Test
-  public void deleteDocumentTestB() throws NotConnectedException, InternalException {
+  // @Test
+  // public void deleteDocumentTestB() throws NotConnectedException, InternalException {
 
-    Kuzzle kuzzleMock = spy(new Kuzzle(networkProtocol));
-    String index = "nyc-open-data";
-    String collection = "yellow-taxi";
+  //   Kuzzle kuzzleMock = spy(new Kuzzle(networkProtocol));
+  //   String index = "nyc-open-data";
+  //   String collection = "yellow-taxi";
 
-    ArgumentCaptor arg = ArgumentCaptor.forClass(KuzzleMap.class);
+  //   ArgumentCaptor arg = ArgumentCaptor.forClass(KuzzleMap.class);
 
-    kuzzleMock.getDocumentController().delete(index, collection, "some-id");
-    Mockito.verify(kuzzleMock, Mockito.times(1)).query((KuzzleMap) arg.capture());
+  //   kuzzleMock.getDocumentController().delete(index, collection, "some-id");
+  //   Mockito.verify(kuzzleMock, Mockito.times(1)).query((KuzzleMap) arg.capture());
 
-    assertEquals(((KuzzleMap) arg.getValue()).getString("controller"), "document");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("action"), "delete");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("index"), "nyc-open-data");
-    assertEquals(((KuzzleMap) arg.getValue()).getString("_id"), "some-id");
-    assertEquals(((KuzzleMap) arg.getValue()).getBoolean("waitForRefresh"), null);
-  }
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("controller"), "document");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("action"), "delete");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("index"), "nyc-open-data");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getString("_id"), "some-id");
+  //   assertEquals(((KuzzleMap) arg.getValue()).getBoolean("waitForRefresh"), null);
+  // }
 
-  @Test(expected = NotConnectedException.class)
-  public void deleteDocumentShouldThrowWhenNotConnected() throws NotConnectedException, InternalException {
-    AbstractProtocol fakeNetworkProtocol = Mockito.mock(WebSocket.class);
-    Mockito.when(fakeNetworkProtocol.getState()).thenAnswer((Answer<ProtocolState>) invocation -> ProtocolState.CLOSE);
+  // @Test(expected = NotConnectedException.class)
+  // public void deleteDocumentShouldThrowWhenNotConnected() throws NotConnectedException, InternalException {
+  //   AbstractProtocol fakeNetworkProtocol = Mockito.mock(WebSocket.class);
+  //   Mockito.when(fakeNetworkProtocol.getState()).thenAnswer((Answer<ProtocolState>) invocation -> ProtocolState.CLOSE);
 
-    Kuzzle kuzzleMock = spy(new Kuzzle(fakeNetworkProtocol));
-    String index = "nyc-open-data";
-    String collection = "yellow-taxi";
+  //   Kuzzle kuzzleMock = spy(new Kuzzle(fakeNetworkProtocol));
+  //   String index = "nyc-open-data";
+  //   String collection = "yellow-taxi";
 
-    kuzzleMock.getDocumentController().delete(index, collection, "some-id");
-  }
+  //   kuzzleMock.getDocumentController().delete(index, collection, "some-id");
+  // }
 }
