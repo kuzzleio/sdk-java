318dec20e77a924fbf72b0e67f92b55101d2c2c8
jenow <jenova.ke20@gmail.com>
2020-03-09 11:28:00 +0100

    fix count

--- src/main/java/io/kuzzle/sdk/API/Controllers/RealtimeController.java
+++ src/main/java/io/kuzzle/sdk/API/Controllers/RealtimeController.java
@@ -85,7 +85,7 @@ public class RealtimeController extends BaseController {
         .query(new KuzzleMap()
             .put("controller", "realtime")
             .put("action", "count")
-            .put("body", new KuzzleMap().put("room_id", roomId)))
+            .put("body", new KuzzleMap().put("roomId", roomId)))
         .thenApplyAsync((response) -> ((KuzzleMap) response.result).getNumber("count").intValue());
   }
 
