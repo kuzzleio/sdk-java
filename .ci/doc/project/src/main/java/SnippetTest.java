9f6823e0f62279b5c2c6c666467f5b1590646bb4
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-03-11 14:50:30 +0100

    mistake

--- .ci/doc/project/src/main/java/SnippetTest.java
+++ .ci/doc/project/src/main/java/SnippetTest.java
@@ -1,13 +1,13 @@
 import io.kuzzle.sdk.Kuzzle;
+import java.util.ArrayList;
 import io.kuzzle.sdk.Protocol.WebSocket;
 import io.kuzzle.sdk.Options.Protocol.WebSocketOptions;
 import io.kuzzle.sdk.Options.KuzzleOptions;
+import java.util.concurrent.ConcurrentHashMap;
+import io.kuzzle.sdk.CoreClasses.Responses.Response;
+import io.kuzzle.sdk.Options.SubscribeOptions;
 import io.kuzzle.sdk.Options.UpdateOptions;
 import io.kuzzle.sdk.Options.CreateOptions;
-import io.kuzzle.sdk.CoreClasses.Responses.Response;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.ArrayList;
 
 public class SnippetTest {
   private static Kuzzle kuzzle;
@@ -16,8 +16,13 @@ public class SnippetTest {
     try {
       kuzzle = new Kuzzle(new WebSocket("kuzzle"));
       kuzzle.connect();
-      Boolean result = kuzzle.getIndexController().exists("nyc-open-data").get();
-      System.out.println(result.toString());
+      ConcurrentHashMap<String, Object> credentials = new ConcurrentHashMap<>();
+      credentials.put("username", "foo");
+      credentials.put("password", "bar");
+
+      ConcurrentHashMap<String, Object> response = kuzzle.getAuthController().login("local", credentials).get();
+      kuzzle.getAuthController().deleteMyCredentials("local").get();
+      System.out.println("Success");
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
