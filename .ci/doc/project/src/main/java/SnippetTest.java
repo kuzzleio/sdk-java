2f633a4133503b65b65e2c0276b5fccfa9e0c4eb
Yoann Abbes <yoann1.abbes@epitech.eu>
2020-03-11 13:29:46 +0100

    snippet

---
+++ .ci/doc/project/src/main/java/SnippetTest.java
@@ -0,0 +1,29 @@
+import io.kuzzle.sdk.Kuzzle;
+import io.kuzzle.sdk.Protocol.WebSocket;
+import io.kuzzle.sdk.Options.Protocol.WebSocketOptions;
+import io.kuzzle.sdk.Options.KuzzleOptions;
+import io.kuzzle.sdk.Options.UpdateOptions;
+import io.kuzzle.sdk.Options.CreateOptions;
+import io.kuzzle.sdk.CoreClasses.Responses.Response;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+
+public class SnippetTest {
+  private static Kuzzle kuzzle;
+
+  public static void main(String[] argv) {
+    try {
+      kuzzle = new Kuzzle(new WebSocket("kuzzle"));
+      kuzzle.connect();
+      Boolean result = kuzzle.getIndexController().exists("nyc-open-data").get();
+      System.out.println(result.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (kuzzle != null) {
+        kuzzle.disconnect();
+      }
+    }
+  }
+}
