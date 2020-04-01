e403d4d13144a3f2bdaf3b85b5f4cc7175120030
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2020-04-01 09:43:19 +0200

    Add collection:create (#79)
    This PR implements the collection:create method with its unit tests.

--- src/main/java/io/kuzzle/sdk/API/Controllers/CollectionController.java
+++ src/main/java/io/kuzzle/sdk/API/Controllers/CollectionController.java
@@ -41,6 +41,51 @@ public class CollectionController extends BaseController {
   }
 
   /**
+   * Creates a collection in a given index.
+   *
+   * @param index
+   * @param collection
+   * @param mappings
+   * @return a CompletableFuture
+   * @throws NotConnectedException
+   * @throws InternalException
+   */
+  public CompletableFuture<Void> create(
+      final String index,
+      final String collection,
+      final ConcurrentHashMap<String, Object> mappings) throws NotConnectedException, InternalException {
+
+    final KuzzleMap query = new KuzzleMap();
+
+    query
+        .put("index", index)
+        .put("collection", collection)
+        .put("controller", "collection")
+        .put("action", "create")
+        .put("body", mappings != null ? new KuzzleMap(mappings) : null);
+
+    return kuzzle
+        .query(query)
+        .thenApplyAsync((response) -> null);
+  }
+
+  /**
+   * Creates a collection in a given index.
+   *
+   * @param index
+   * @param collection
+   * @return a CompletableFuture
+   * @throws NotConnectedException
+   * @throws InternalException
+   */
+  public CompletableFuture<Void> create(
+      final String index,
+      final String collection) throws NotConnectedException, InternalException {
+
+    return this.create(index, collection, null);
+  }
+
+  /**
    * Get collection mapping
    *
    * @param index
